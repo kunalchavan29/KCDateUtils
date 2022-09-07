852c4e06b27ebeb14737cb4aa5fbd51a7f198e6d
Kunal Chavan <kunal.chavan@t-systems.com>
2022-09-07 20:50:25 +0530

    initial setup

---
+++ KCDateUtils/KCDateUtils.h
@@ -0,0 +1,18 @@
+//
+//  KCDateUtils.h
+//  KCDateUtils
+//
+//  Created by Kunal Chavan on 07/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KCDateUtils.
+FOUNDATION_EXPORT double KCDateUtilsVersionNumber;
+
+//! Project version string for KCDateUtils.
+FOUNDATION_EXPORT const unsigned char KCDateUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCDateUtils/PublicHeader.h>
+
+
---
+++ KCDateUtilsExample/KCDateUtilsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KCDateUtilsExampleApp.swift
+//  KCDateUtilsExample
+//
+//  Created by A118830248 on 07/09/22.
+//
+
+import SwiftUI
+
+@main
+struct KCDateUtilsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
